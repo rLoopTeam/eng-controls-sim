0dcc259291f2454409e6aa4783036a14cc34cbb3
Sean Marquez <capsulecorplab@gmail.com>
2017-02-09 16:25:23 -0800

    added Hover-engine dynamics due to track oscillation

--- Fhoverlift.m
+++ Fhoverlift.m
@@ -1,7 +1,9 @@
+% Lift force for 1 hover-engine
+
 % Input: velocity (m/s), brakegap (mm), hover height (mm)
 % Output: Hover lift force (N)
 function F = Fhoverlift(v,z,RPM)
     %%%% Do you even 'lift', bro? %%%%
-    F = 1142 * exp(-99.144*z) * atan(0.089501*(v + 0.00932005*RPM));
+    F = 1142 * exp(-99.144*10^(-3)*z) * atan(0.089501*(v + 0.00932005*RPM));
     
 end
