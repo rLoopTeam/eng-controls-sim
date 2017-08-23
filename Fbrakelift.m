d60a20de694a6b933019f33a1d0385b11c383d2f
saiyaman <capsulecorplab@gmail.com>
2017-08-23 03:13:31 -0700

    Added Tbrakeload.m and updated Fbrakelift.m to account for lift due to magnetic repulsion function

--- Fbrakelift.m
+++ Fbrakelift.m
@@ -1,5 +1,5 @@
 % Input: velocity (m/s), brakegap (mm)
-% Output: Brake lift force for one brakepad (N)
+% Output: Brake lift force for one brakepad due to magnetic repulsion between brakes and eddy currents (N)
 function F = Fbrakelift(v,bg)
     % lift due to eddy currents
     F_eddylift = 3265.1 * exp(-209.4*bg*10^-3) * log(v + 1) - 2636.7*exp(-207*bg*10^-3) * (v + 0.6) * exp(-0.16*v);
